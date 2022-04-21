cff6eaea8257dc59385a86236d3c4ba4a772729a
jcalvolo <jcalvolo@emeal.nttdata.com>
2022-04-21 17:40:07 +0200

    Segunda subida ahora con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { HeroesModules } from "./heroes/heroes.module";
+import { ContadorModule } from "./contador/contador.module";
 
-import { AppComponent } from './app.component';
-import { ContadorModule } from './contador/contador.module';
-
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { HeroesModules } from './heroes/heroes.module';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { NgModule } from "@angular/core";
 
 @NgModule({
   declarations: [
